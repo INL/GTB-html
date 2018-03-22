4de75f252c1e550a0353dddeff67f48f1db4fcf0
Jan Niestadt <jan.niestadt@ivdnt.org>
2018-03-22 15:16:15 +0100

    URLs without protocol and servername.

--- maak-test.sh
+++ maak-test.sh
@@ -8,10 +8,10 @@ SAVEPWD=`pwd`
 
 cd generate-gtb-html-files
 ./generate-gtb-html-files.sh \
-  "BASEARTICLEURL=http://gtb.ato.inl.nl/iWDB/search?actie=article" \
-  "BASEARTICLECONTENTURL=http://gtb.ato.inl.nl/iWDB/search?actie=article_content" \
-  "BASESEARCHURL=http://gtb.ato.inl.nl/iWDB/search?actie=results" \
-  "BASELISTURL=http://gtb.ato.inl.nl/iWDB/search?actie=list" \
+  "BASEARTICLEURL=/iWDB/search?actie=article" \
+  "BASEARTICLECONTENTURL=/iWDB/search?actie=article_content" \
+  "BASESEARCHURL=/iWDB/search?actie=results" \
+  "BASELISTURL=/iWDB/search?actie=list" \
   "GA_TRACKING_CODE=UA-57793092-1"
   
 cd "$SAVEPWD"
