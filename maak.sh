d600ec9f807e1502f54f46cd5347979d256661fc
Jan Niestadt <jan.niestadt@ivdnt.org>
2021-02-02 13:40:43 +0100

    Simplify the build script situation.

--- maak.sh
+++ maak.sh
@@ -1,1 +1,18 @@
-maak-dev.sh
+#!/bin/sh
+
+# Alvorens dit script uit te voeren, cd naar het directory waar het zich bevindt.
+
+export BASE_TARGET_DIR=target
+
+SAVEPWD=`pwd`
+
+cd generate-gtb-html-files
+./generate-gtb-html-files.sh \
+  "BASEARTICLEURL=/iWDB/search?actie=article" \
+  "BASEARTICLECONTENTURL=/iWDB/search?actie=article_content" \
+  "BASESEARCHURL=/iWDB/search?actie=results" \
+  "BASELISTURL=/iWDB/search?actie=list" \
+  "GA_TRACKING_CODE=UA-57793092-1"
+  
+cd "$SAVEPWD"
+rm "$BASE_TARGET_DIR/redirect.php"
